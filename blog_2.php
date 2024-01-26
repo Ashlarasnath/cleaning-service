62bc050b11a74fd81cbebf3b8fad2435ec7c3870
Ashlarasnath <asmiyasiyam22@gmail.com>
2024-01-26 14:31:32 +0300

    added blog page

---
+++ blog_2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blog1</title>
+    <style>
+        .blog-h1 {
+            text-align: center;
+        }
+
+        .image-container {
+            text-align: center;
+        }
+    </style>    
+</head>
+<body>
+    <div class="image-container">
+        <img src="https://homemaidbetter.com/wp-content/uploads/2018/06/house-cleaning-2.jpg" style="width:80%">
+    </div>
+    <br>
+    <h1 class="blog-h1">The Importance of Deep Cleaning Services for a Healthy Home</h1>
+    <p>
+        When it comes to car dealerships, a great first impression is a must. Customers will expect to see a well-maintained showroom and shiny vehicles when they enter your business. Buying a new vehicle is a large purchase that most people put a lot of thought and consideration into before making their decision. A dirty or[...]
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure a quia explicabo illum deleniti doloribus exercitationem ea sed nostrum, recusandae, quasi, eaque inventore omnis corrupti magnam ipsum eveniet obcaecati. <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet laboriosam perspiciatis tempora ex ducimus reprehenderit quos quod quidem quae officia qui, natus veniam cumque? Nisi, voluptates. Laudantium placeat a iste.
+    </p>
+</body>
+</html>
---
+++ blog_8.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blog1</title>
+    <style>
+        .blog-h1 {
+            text-align: center;
+        }
+
+        .image-container {
+            text-align: center;
+        }
+    </style>    
+</head>
+<body>
+    <div class="image-container">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8ykmZRkSZdR6bFWvvaOk6ckSO2Zl2kbiFgQ&usqp=CAU" style="width:80%">
+    </div>
+    <br>
+    <h1 class="blog-h1">Safety Tips for Cleaning Around Pets</h1>
+    <p>
+    Giving our house or apartment a good cleaning not only makes us feel better, acting as a mood booster and de-stresser, but also a clean abode is physically healthier for us. When we disinfect our living spaces, we eliminate dirt and germs that can make us sick, protect ourselves and loved ones against... 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure a quia explicabo illum deleniti doloribus exercitationem ea sed nostrum, recusandae, quasi, eaque inventore omnis corrupti magnam ipsum eveniet obcaecati. <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet laboriosam perspiciatis tempora ex ducimus reprehenderit quos quod quidem quae officia qui, natus veniam cumque? Nisi, voluptates. Laudantium placeat a iste.
+    </p>
+</body>
+</html>
